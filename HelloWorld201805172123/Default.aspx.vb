c005a6d19278354a4eb8f31b139d59bc7bb6789c
kazbow <39369589+kazbow@users.noreply.github.com>
2018-05-17 21:26:55 +0900

    hi
    hi

---
+++ HelloWorld201805172123/Default.aspx.vb
@@ -0,0 +1,8 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Label1.Text = "Hello World"
+    End Sub
+End Class
